c45156bdf46d84cab2c94a6554205b26230098bd
AlejandroHinojosaRubio <hinojosa.rub@gmail.com>
2024-07-15 17:47:16 +0200

    Chapters 1 and 2

---
+++ src/coffees/coffees.controller.spec.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoffeesController } from './coffees.controller';
+
+describe('CoffeesController', () => {
+  let controller: CoffeesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CoffeesController],
+    }).compile();
+
+    controller = module.get<CoffeesController>(CoffeesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
