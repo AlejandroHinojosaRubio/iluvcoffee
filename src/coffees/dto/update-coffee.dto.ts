0c0b2d51651f5d3cbc1056573c91e4496aaa667b
AlejandroHinojosaRubio <hinojosa.rub@gmail.com>
2025-03-03 11:56:51 +0100

    Chapter 7

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,4 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateCoffeeDto } from "./create-coffee.dto";
+import { PartialType } from '@nestjs/swagger';
+import { CreateCoffeeDto } from './create-coffee.dto';
 
-export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto){}
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
